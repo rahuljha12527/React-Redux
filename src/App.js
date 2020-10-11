da3d6fb258468a1e42102ee1ffd0a4ca3cecef82
rahuljha661 <rahuljha12527@gmail.com>
2020-10-12 00:39:32 +0530

    add resux logger

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import store from './redux/store'
 import { Provider } from "react-redux";
 import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
+import IceCreamContainer from "./components/IceCreamContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+      <IceCreamContainer/>
          <CakeContainer /> 
         <HooksCakeContainer />
+        {/* <iceCreamContainer /> */}
+       
       </div>
     </Provider>
   );
