74bf68aafb060fcc2927136482ae29bf7b6a6abb
rahuljha661 <rahuljha12527@gmail.com>
2020-10-11 23:54:16 +0530

    add commpit file

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import store from './redux/store'
 import { Provider } from "react-redux";
 import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <CakeContainer />
+         <CakeContainer /> 
+        <HooksCakeContainer />
       </div>
     </Provider>
   );
