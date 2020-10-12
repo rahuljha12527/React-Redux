b0b9ecf1635c0f2d41ef626fda4c50bea7f85c7c
rahuljha661 <rahuljha12527@gmail.com>
2020-10-12 12:30:05 +0530

    add cakecontainer

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import { Provider } from "react-redux";
 import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake/>
+        <ItemContainer />
       <IceCreamContainer/>
          <CakeContainer /> 
         <HooksCakeContainer />
         {/* <iceCreamContainer /> */}
+        <NewCakeContainer />
        
       </div>
     </Provider>
--- src/redux/cakes/cakeReducer.js
+++ src/redux/cakes/cakeReducer.js
@@ -7,9 +7,8 @@ import {BUY_CAKE} from './cakeTypes'
    switch(action.type){
       case BUY_CAKE:  return{
           ...state,
-          numOfCakes:state.numOfCakes-1
+          numOfCakes:state.numOfCakes-action.payload
       }
-
       default: return state
 
    }
